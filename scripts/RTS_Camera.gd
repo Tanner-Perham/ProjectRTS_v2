92dda0c1a9638404395cd353df6f66ec4eb0b9ef
Tanner Perham <tanner.perham@gmail.com>
2024-07-14 22:31:30 +0800

    Basic unit movement - using Navmesh to avoid obstacles

--- scripts/RTS_Camera.gd
+++ scripts/RTS_Camera.gd
@@ -92,3 +92,15 @@ func camera_automatic_pan(delta: float) -> void:
 	
 func get_Vector2_from_Vector3(unproject_from_vec3:Vector3) -> Vector2:
 	return camera.unproject_position(unproject_from_vec3)
+
+func get_vector3_from_camera_raycast(camera_2D_coordinates:Vector2) -> Vector3:
+	var ray_from:Vector3 = camera.project_ray_origin(camera_2D_coordinates)
+	var ray_to:Vector3 = ray_from + camera.project_ray_normal(camera_2D_coordinates) * 1000.0
+	var ray_parameters:PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(ray_from, ray_to)
+
+	var result:Dictionary = camera.get_world_3d().get_direct_space_state().intersect_ray(ray_parameters)
+	print(result)
+	if result:
+		return result.position
+	else:
+		return Vector3.ZERO
--- scripts/test_unit.gd
+++ scripts/test_unit.gd
@@ -1,7 +1,19 @@
 extends Node3D
 
+# NODES
 @onready var selected_graphic:Sprite3D = $selected
+@onready var unit_graphic:Node3D = $Test_Unit_01
+@onready var character_body:CharacterBody3D = $CharacterBody3D
+@onready var map_RID:RID = get_world_3d().get_navigation_map()
+
 @export var player_owner: String
+
+var pathing:bool = false
+var pathing_point:int = 0
+var path_points_packed:PackedVector3Array
+
+# OBJ ATTRIBUTES / DATA
+var obj_data:Dictionary = {"SPEED": 8.0}
 
 func _ready() -> void:
 	unselected()
@@ -11,3 +23,22 @@ func selected() -> void:
 
 func unselected() -> void:
 	selected_graphic.visible = false
+	
+func unit_path_new(goal_position: Vector3) -> void:
+	var safe_goal:Vector3 = NavigationServer3D.map_get_closest_point(map_RID, goal_position)
+	path_points_packed = NavigationServer3D.map_get_path(map_RID, global_position, safe_goal, true)
+	pathing = true
+	pathing_point = 0
+
+func _physics_process(delta: float) -> void:
+	if pathing:
+		var path_next_point:Vector3 = path_points_packed[pathing_point] - global_position
+		if path_next_point.length_squared() > 1.0:
+			var velocity:Vector3 = (path_next_point.normalized() * delta) * obj_data["SPEED"]
+			global_position += velocity
+		else:
+			if pathing_point < (path_points_packed.size() - 1):
+				pathing_point += 1 # Grab next path point
+				_physics_process(delta)
+			else:
+				pathing = false 
